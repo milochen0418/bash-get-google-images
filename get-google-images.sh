207b99f5f2c9789da414835d72b7f90a5b0b2630
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-14 19:10:00 +0000

    implement get-google-images.sh

--- get-google-images.sh
+++ get-google-images.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+
+## Usage
+#get-google-images.sh [keyword] [number]
+
+if ! which google-chrome-stable; then
+	echo "Command is interrupted because you have no google-chrome-stable in any folder of PATH"
+	echo "Suggest you to debug ./install-google-chrome-stable.sh script , or you can install chrome by yourself"
+	exit 1
+fi
+
+if ! which chromedriver; then
+	echo "Command is interrupted because you have no chromedriver in any folder of  PATH"
+	echo "Suggest you to debug ./install.sh script , or you can install chromedriver by yourself"
+	exit 1
+fi
+
+if ! which googleimagesdownload; then
+	echo "Command is interrupted because you have no chromedriver in any folder of  PATH"
+	echo "Suggest you to debug ./install.sh script , or you can install googleimagesdownload by yourself"
+	exit 1
+fi
+
+googleimagesdownload --keywords "$1" --limit $2 --chromedriver=$(which chromedriver)
+
+
+
